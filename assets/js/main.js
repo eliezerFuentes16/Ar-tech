8594bdd8d0bc05001e6a4358c1348de8fb95fc41
Eliezer Fuentes <eliezermario008@gmail.com>
2022-08-06 20:34:17 -0400

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 (function ($) {
     "use strict";
+$('.sticky-header').addClass('sticky');
 
  /*----------------------------------------
   Sticky Header Activation
@@ -8,7 +9,7 @@
     if ($(this).scrollTop() > 0) {
       $('.sticky-header').addClass('sticky');
     } else {
-      $('.sticky-header').removeClass('sticky');
+      // $('.sticky-header').removeClass('sticky');
     }
   });
 
@@ -140,3 +141,31 @@ function parallax(e){
     });
 
 })(jQuery);
+
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    center: true,
+    navText: [
+        "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i>"
+    ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+  });
+});
